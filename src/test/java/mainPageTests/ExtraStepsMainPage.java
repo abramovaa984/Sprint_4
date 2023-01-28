96ba601d1a043f4e552847565109e791bbbaea2e
abramovaa984 <abramovaa984@gmail.com>
2023-01-28 22:39:16 +0300

    added project files

---
+++ src/test/java/mainPageTests/ExtraStepsMainPage.java
@@ -0,0 +1,28 @@
+package mainPageTests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ExtraStepsMainPage {
+    WebDriver chromeDriver;
+    WebDriver firefoxDriver;
+
+
+
+    @Before
+    public void setUp() {
+        chromeDriver = new ChromeDriver();
+        firefoxDriver = new FirefoxDriver();
+    }
+
+    @After
+    public void teardown() {
+        // Закрыть браузер
+        chromeDriver.quit();
+        firefoxDriver.quit();
+    }
+
+}
